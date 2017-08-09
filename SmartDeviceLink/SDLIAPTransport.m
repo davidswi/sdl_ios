bf9ffb904bde4e0cee26e35493d8f2c4ff3b68b8
David Switzer <dswitzer@xevo.com>
2017-08-09 15:03:20 -0700

    Based on code review and experimental analysis, compute connection delay for IAP using crypto random number generator.

--- SmartDeviceLink/SDLIAPTransport.m
+++ SmartDeviceLink/SDLIAPTransport.m
@@ -450,40 +450,20 @@ int const streamOpenTimeoutSeconds = 2;
     const double min_value = 1.5;
     const double max_value = 9.5;
     double range_length = max_value - min_value;
-
-    static double delay = 0;
-
-    // HAX: This pull the app name and hashes it in an attempt to provide a more even distribution of retry delays. The evidence that this does so is anecdotal. A more ideal solution would be to use a list of known, installed SDL apps on the phone to try and deterministically generate an even delay.
-    if (delay == 0) {
-        NSString *appName = [[NSProcessInfo processInfo] processName];
-        if (appName == nil) {
-            appName = @"noname";
-        }
-
-        // Run the app name through an md5 hasher
-        const char *ptr = [appName UTF8String];
-        unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
-        CC_MD5(ptr, (unsigned int)strlen(ptr), md5Buffer);
-
-        // Generate a string of the hex hash
-        NSMutableString *output = [NSMutableString stringWithString:@"0x"];
-        for (int i = 0; i < 8; i++) {
-            [output appendFormat:@"%02X", md5Buffer[i]];
-        }
-
+    double delay = min_value;
+    UInt64 randomLong;
+    
+    int ret = SecRandomCopyBytes(kSecRandomDefault, sizeof(UInt64), (uint8_t *)&randomLong);
+    if (ret == 0) {
         // Transform the string into a number between 0 and 1
-        unsigned long long firstHalf;
-        NSScanner *pScanner = [NSScanner scannerWithString:output];
-        [pScanner scanHexLongLong:&firstHalf];
-        double hashBasedValueInRange0to1 = ((double)firstHalf) / 0xffffffffffffffff;
-
+        double randomValueInRange0to1 = ((double)randomLong) / 0xffffffffffffffff;
+        
         // Transform the number into a number between min and max
-        delay = ((range_length * hashBasedValueInRange0to1) + min_value);
+        delay = ((range_length * randomValueInRange0to1) + min_value);
     }
-
+    
     return delay;
 }
-
 
 #pragma mark - Lifecycle Destruction
 
